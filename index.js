49a593e5044350ac526a79f2cedff551d5167aa5
Wawezz <tikobaghdasaryan9@gmail.com>
2019-03-18 19:49:43 +0300

    date fix

--- index.js
+++ index.js
@@ -35,44 +35,6 @@ const sleep = (ms) => {
 const con = async () => {
     try {
         await sql.connect('mssql://' + mssqlConf.user + ':' + mssqlConf.password + '@' + mssqlConf.host + '/' + mssqlConf.db)
-        const resultNotes = await sql.query`select * from placesNotelog`
-
-        const url = 'mongodb://' + mongoConf.host + ':' + mongoConf.port + '/';
-
-        const notes = resultNotes.recordset;
-        let notesObj = [];
-
-        for (let n = 0; n < notes.length; n++) {
-            let note = notes[n];
-            notesObj.push({
-                _id: uuidv1(),
-                elementId: note.PlaceID,
-                elementType: 16,
-                noteType: 6,
-                createdBy: userId,
-                createdAt: moment(note.date).unix(),
-                updatedAt: moment(note.date).unix(),
-                dataValue: { workerId: note.update_worker_id }
-            });
-        }
-
-        mongodb.connect(url, {
-            useNewUrlParser: true,
-            auth: {
-                user: mongoConf.user,
-                password: mongoConf.password,
-            }
-        }, function (err, db) {
-            if (err) throw err;
-            const dbo = db.db(mongoConf.db);
-            dbo.collection(mongoConf.collection).insertMany(notesObj, function (err, res) {
-                if (err) throw err;
-                console.log("Number of documents inserted: " + res.insertedCount);
-                db.close();
-            });
-        });
-
-        await sleep(100);
 
         const result = await sql.query`select * from places left join placeInfo on places.placeID = placeInfo.placeId left join placeDetails on places.placeID = placeDetails.placeID`
 
@@ -82,10 +44,12 @@ const con = async () => {
 
         const records = result.recordset;
 
+        let IDs = [];
         let query = [];
         let updateQuery = [];
         let defaultSql = 'INSERT INTO app_place_leads (id, geo) VALUES ';
         for (let i = 0; i < records.length; i++) {
+
             let points = null;
             let record = records[i];
             if (record.geo) {
@@ -93,6 +57,8 @@ const con = async () => {
 
                 points = "ST_GeomFromText('POINT(" + point.x + " " + point.y + ")') ";
             }
+
+            IDs.push(record.placeID[0]);
 
             let sqlR = '';
             sqlR += '( ';
@@ -121,8 +87,8 @@ const con = async () => {
                 userId,
                 moment(record.InsertDate).format("YYYY-MM-DD HH:mm:ss"),
                 moment(record.lastUpdate).format("YYYY-MM-DD HH:mm:ss"),
-                parseInt(moment(record.contractDate).format("YYYY")) >= 1970 ? record.contractDate : null,
-                parseInt(moment(record.NextFollowupDate).format("YYYY")) >= 1970 ? record.NextFollowupDate : null
+                parseInt(moment(record.contractDate).format("YYYY")) >= 1970 ? moment(record.contractDate).format("YYYY-MM-DD HH:mm:ss") : null,
+                parseInt(moment(record.NextFollowupDate).format("YYYY")) >= 1970 ? moment(record.NextFollowupDate).format("YYYY-MM-DD HH:mm:ss") : null
             ]);
 
             if ((i + 1) % 1000 === 0) {
@@ -162,6 +128,48 @@ const con = async () => {
 
             updateQuery = [];
         }
+
+        await sleep(100);
+
+        const resultNotes = await sql.query`select * from placesNotelog`
+
+        const url = 'mongodb://' + mongoConf.host + ':' + mongoConf.port + '/';
+
+        const notes = resultNotes.recordset;
+        let notesObj = [];
+
+        for (let n = 0; n < notes.length; n++) {
+            let note = notes[n];
+
+            if (IDs.includes(note.PlaceID)) {
+                notesObj.push({
+                    _id: uuidv1(),
+                    elementId: note.PlaceID,
+                    elementType: 16,
+                    noteType: 6,
+                    createdBy: userId,
+                    createdAt: moment(note.date).unix(),
+                    updatedAt: moment(note.date).unix(),
+                    dataValue: { workerId: note.update_worker_id }
+                });
+            }
+        }
+
+        mongodb.connect(url, {
+            useNewUrlParser: true,
+            auth: {
+                user: mongoConf.user,
+                password: mongoConf.password,
+            }
+        }, function (err, db) {
+            if (err) throw err;
+            const dbo = db.db(mongoConf.db);
+            dbo.collection(mongoConf.collection).insertMany(notesObj, function (err, res) {
+                if (err) throw err;
+                console.log("Number of documents inserted: " + res.insertedCount);
+                db.close();
+            });
+        });
 
         console.log('end');
 
